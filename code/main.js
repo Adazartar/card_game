63ee3ae66e17433e071667407c3c78402d16532b
mantelloa <adam.mantello@student.unimelb.edu.au>
2023-06-22 14:21:39 +1000

    fixed up the layout, tried to sort out the size of the canvas

--- code/main.js
+++ code/main.js
@@ -10,3 +10,21 @@ console.log(addRepeatedly(2,8));
 console.log(cards.card1.cost);
 printToConsole();
 
+
+/////////////////////////////
+
+const height = 640;
+const width = 960;
+
+var canvas = document.getElementById("game");
+
+var screenHeightRatio = window.innerHeight / height;
+var screenWidthRatio = window.innerWidth / width;
+
+canvas.height = height;
+canvas.width = width;
+
+var ctx = canvas.getContext("2d");
+ctx.fillStyle = "red";
+ctx.fillRect(0, 0, width, height);
+
